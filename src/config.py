2f5c24f4ecc99b50de35ab31cefb4181ba78533c
Nicholas Goll <nicholastgoll@gmail.com>
2026-03-03 15:34:51 -0600

    Revert "added to config and game"
    This reverts commit dffb71df31a5fa0a6dae40698c380b2c06ae1e68.

--- src/config.py
+++ src/config.py
@@ -1,13 +1,8 @@
 '''
-The purpose of this file is to provide constants to other 
-files that are easily adjustable instead of hardcoding in those files.
+The purpose of this file is to provide constants to other files that are easily adjustable instead of hardcoding in those files.
 '''
 
 # DISTANCE SENSOR CONSTANTS
 ECHO_PIN = 18
 TRIGGER_PIN = 17
 MAX_DISTANCE = 0.5 # MUST TEST THIS AND ADJUST AS NEEDED
-
-# GAME CONSTANTS
-DISPLAY = (600,800)
-FPS = 60
