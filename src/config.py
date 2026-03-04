22798d482a573da0217e536e644a2e22767a2871
Nicholas Goll <nicholastgoll@gmail.com>
2026-03-03 23:32:27 -0600

    added random library

--- src/config.py
+++ src/config.py
@@ -2,6 +2,8 @@
 The purpose of this file is to provide constants to other files that are easily adjustable instead of hardcoding in those files.
 '''
 
+import random
+
 # distance_sensor.py *
 ECHO_PIN = 18
 TRIGGER_PIN = 17
@@ -11,8 +13,8 @@ MAX_DISTANCE = 0.5 # MUST TEST THIS AND ADJUST AS NEEDED
 DISPLAY = (600, 800)
 FPS = 60
 # ball movement
-BALL_SPEED_X = 6
-BALL_SPEED_Y = 6
+BALL_SPEED_X = 6 * random.choice((1, -1))
+BALL_SPEED_Y = 6 * random.choice((1, -1))
 # player movement
 PLAYER_SPEED = 0
 # opponent movement
