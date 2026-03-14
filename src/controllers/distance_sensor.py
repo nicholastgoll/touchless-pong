133db12d180d79bd96b6b55b168f692b73b8c106
Nicholas Goll <nicholastgoll@gmail.com>
2026-03-14 15:36:44 -0500

    push to RPi

--- src/controllers/distance_sensor.py
+++ src/controllers/distance_sensor.py
@@ -16,7 +16,7 @@ class SensorController():
             pin_factory=factory
         )
         
-        self.readings = deque(maxlen=10)
+        self.readings = deque(maxlen=5)
         
     # returns float 0.0-1 where 1 is MAX_DISTANCE
     def get_paddle_pos(self):
