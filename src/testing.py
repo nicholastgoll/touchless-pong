71e1e735301e6fefec68eeead0933bf7dec34fb4
Nicholas Goll <nicholastgoll@gmail.com>
2026-03-15 17:48:33 -0500

    push to pi

--- src/testing.py
+++ src/testing.py
@@ -1,7 +1,8 @@
 from gpiozero import LED
+from gpiozero.pins.pigpio import PiGPIOFactory
 from time import sleep
 
-led = LED(22)
+led = LED(22, pin_factory=PiGPIOFactory())
 led.on()
 print("LED should be solid green for 10 seconds...")
 sleep(10)
